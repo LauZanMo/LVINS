d7d077ebe85e7cbaa0d991bafcd317f783b93766
LauZanMo <LauZanMo@whu.edu.cn>
2025-04-11 10:47:12 +0800

    初次提交配置包

---
+++ lvins_config/cmake/lvins_setup.cmake
@@ -0,0 +1,24 @@
+macro(lvins_setup)
+    # 设置编译选项
+    set(CMAKE_CXX_STANDARD 17)
+    
+    if (NOT CMAKE_BUILD_TYPE)
+        set(CMAKE_BUILD_TYPE Release)
+    endif ()
+    
+    if (CMAKE_BUILD_TYPE MATCHES Release)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3")
+    elseif (CMAKE_BUILD_TYPE MATCHES RelWithDebInfo)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O3")
+    elseif (CMAKE_BUILD_TYPE MATCHES Debug)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -O0")
+    endif ()
+    
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wpedantic")
+    
+    if (CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "arm")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon")
+    elseif (CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "x86_64")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse -msse2 -msse3 -mssse3 -msse4.1 -msse4.2")
+    endif ()
+endmacro()
---
+++ lvins_config/lvins_config.cmake
@@ -0,0 +1,23 @@
+# 设置默认配置路径
+set(LVINS_CONFIG_DIR "${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}")
+
+# 设置记录等级，trace和debug会执行额外的检查，会影响性能
+# 0:trace, 1:debug, 2:info, 3:warn, 4:error, 5:fatal
+set(LVINS_LOG_LEVEL 0)
+
+# 最大传感器数（仅相机或lidar）
+set(LVINS_MAX_SENSOR_SIZE 16)
+
+# 设置浮点数精度
+set(LVINS_FLOAT_PRECISION float)
+
+# 设置许可证检查
+set(LVINS_LICENSE_CHECK true)
+set(LVINS_PRELOAD_UTC 1747238400)
+set(LVINS_PRELOAD_CPUID 1)
+
+# 生成配置头文件
+configure_file(
+    ${PROJECT_SOURCE_DIR}/include/${PROJECT_NAME}/setup.h.in
+    ${PROJECT_SOURCE_DIR}/include/${PROJECT_NAME}/setup.h
+)
